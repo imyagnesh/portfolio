b5e2cc3d1e2aaf1580660d77b8b27db0c4d00516
Yagnesh Modh <yagneshmodh@Yagneshs-MacBook-Pro-2.local>
2020-08-11 22:11:50 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import abc from './test'
+// import abc, { test1 } from './test'
+// import { test } from './test'
+// import * as abc from './test'
+
+
+console.log(abc.test)
+console.log(abc.test1)
+console.log(abc.test2)
+
+console.log('bac');
+
+console.log(abc);
---
+++ src/test.js
@@ -0,0 +1,9 @@
+export const test = "test string";
+export const test1 = "test1 string";
+export const test2 = "test1 string";
+
+
+export default {
+    test,
+    test1
+}
